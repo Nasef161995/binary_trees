476787bfd17f3b38098d6ec5a1a8daaeb78dc110
Nasef161995~ <mnasef905@gmail.com>
2023-11-28 17:16:19 +0300

    000

--- binary_trees.h
+++ binary_trees.h
@@ -16,10 +16,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+int n;
+struct binary_tree_s *parent;
+struct binary_tree_s *left;
+struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -27,4 +27,4 @@ typedef struct binary_tree_s binary_tree_t;
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-#endif // BINARY_TREES_H
+#endif
