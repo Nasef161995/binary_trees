bbe8ca8f2c59df4dc45feb315e0effcaed538185
Nasef161995~ <mnasef905@gmail.com>
2023-11-29 20:08:11 +0300

    10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - ......
+ * @tree: ....
+ * Return: ....
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth;
+
+	if (tree->parent == NULL || tree == NULL)
+	{
+		return (0);
+	}
+	depth = binary_tree_depth(tree->parent) + 1;
+	return (depth);
+}
