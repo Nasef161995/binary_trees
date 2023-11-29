557f220075b050691dc3e40c415df82d04ad719c
Nasef161995~ <mnasef905@gmail.com>
2023-11-29 18:49:42 +0300

    999

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_height - ......
+ * @tree: ....
+ * Return: ....
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right, height;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	if (left > right)
+	{
+		height = left + 1;
+	}
+	else
+	{
+		height = right + 1;
+	}
+	return (height);
+}
