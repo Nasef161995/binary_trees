ccc8e7601b4830c9b8fa0fb021dbe151b3d8ef06
Nasef161995~ <mnasef905@gmail.com>
2023-11-29 20:53:27 +0300

    11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -16,7 +16,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	if (tree->left == NULL && tree->right == NULL) 
+	if (tree->left == NULL && tree->right == NULL)
 	{
 		return (1);
 	}
