362a8723fa23a3cc5333bdcb463659b9b775990c
Nasef161995~ <mnasef905@gmail.com>
2023-11-29 20:23:35 +0300

    10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,9 +10,9 @@
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth;
-
-	depth = -1;
-	if (tree->parent == NULL || tree == NULL)
+	if (tree == NULL)
+		return (0);
+	if (tree->parent == NULL)
 	{
 		return (0);
 	}
