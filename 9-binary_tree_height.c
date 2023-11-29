dc0852ca4c8ecb0576a1dbb6a31fa8075c07234d
Nasef161995~ <mnasef905@gmail.com>
2023-11-29 19:50:36 +0300

    999

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,8 +15,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (0);
 	}
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (0);
+	}
+
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
+
 	if (left > right)
 	{
 		height = left + 1;
