3e931470910d778554f991fb60dcf60677687ca4
Nasef161995~ <mnasef905@gmail.com>
2023-11-30 22:37:30 +0300

    16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -12,12 +12,17 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	{
 		return (0);
 	}
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
 	if (tree->left && tree->right)
 	{
 		if (binary_tree_balance(tree) == 0 && binary_tree_is_full(tree) == 1)
 		{
 			return (1);
 		}
+
 	}
 	return (0);
 }
